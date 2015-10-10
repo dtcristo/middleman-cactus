98d99d122cf2a8c58fc848c13ec3251bb1ec99ef
David Cristofaro <git@dtcristo.com>
2015-10-11 08:30:19 +1100

    Use new config.rb structure

--- config.rb
+++ config.rb
@@ -1,84 +1,65 @@
 ###
-# Blog settings
-###
-
-# Time.zone = "UTC"
-
-activate :blog do |blog|
-  # This will add a prefix to all links, template references and source paths
-  blog.prefix = "blog"
-
-  blog.permalink = "{title}"
-  # Matcher for blog source files
-  blog.sources = "{year}-{month}-{day}-{title}"
-  # blog.taglink = "tags/{tag}.html"
-  # blog.layout = "layout"
-  # blog.summary_separator = /(READMORE)/
-  # blog.summary_length = 250
-  # blog.year_link = "{year}.html"
-  # blog.month_link = "{year}/{month}.html"
-  # blog.day_link = "{year}/{month}/{day}.html"
-  blog.default_extension = ".md"
-
-  blog.tag_template = "tag.html"
-  # blog.calendar_template = "calendar.html"
-
-  # Enable pagination
-  # blog.paginate = true
-  # blog.per_page = 10
-  # blog.page_link = "page/{num}"
-end
-
-page "/feed.xml", layout: false
-
-###
-# Compass
-###
-
-# Change Compass configuration
-# compass_config do |config|
-#   config.output_style = :compact
-# end
-
-###
 # Page options, layouts, aliases and proxies
 ###
 
 # Per-page layout changes:
 #
 # With no layout
-# page "/path/to/file.html", layout: false
-#
+page '/*.xml', layout: false
+page '/*.json', layout: false
+page '/*.txt', layout: false
+
 # With alternative layout
-# page "/path/to/file.html", layout: :otherlayout
-#
-# A path which all have the same layout
-# with_layout :admin do
-#   page "/admin/*"
-# end
+# page '/path/to/file.html', layout: :otherlayout
 
 # Proxy pages (http://middlemanapp.com/basics/dynamic-pages/)
-# proxy "/this-page-has-no-template.html", "/template-file.html", locals: {
-#  which_fake_page: "Rendering a fake page with a local variable" }
+# proxy '/this-page-has-no-template.html', '/template-file.html', locals: {
+#  which_fake_page: 'Rendering a fake page with a local variable' }
 
 ###
 # Helpers
 ###
 
-# Automatic image dimensions on image_tag helper
-# activate :automatic_image_sizes
+# Time.zone = 'UTC'
+
+activate :blog do |blog|
+  # This will add a prefix to all links, template references and source paths
+  blog.prefix = 'blog'
+
+  blog.permalink = '{title}'
+  # Matcher for blog source files
+  blog.sources = '{year}-{month}-{day}-{title}'
+  # blog.taglink = 'tags/{tag}.html'
+  blog.layout = 'article'
+  # blog.summary_separator = /(READMORE)/
+  # blog.summary_length = 250
+  # blog.year_link = '{year}.html'
+  # blog.month_link = '{year}/{month}.html'
+  # blog.day_link = '{year}/{month}/{day}.html'
+  blog.default_extension = '.md'
+
+  blog.tag_template = 'tag.html'
+  # blog.calendar_template = 'calendar.html'
+
+  # Enable pagination
+  blog.paginate = true
+  blog.per_page = 5
+  # blog.page_link = 'page/{num}'
+end
+
+# Pretty URLs (https://middlemanapp.com/advanced/pretty_urls/)
+activate :directory_indexes
+set :trailing_slash, false
 
 # Reload the browser automatically whenever files change
-# activate :livereload
-
-# Pretty URLs (Remove .html from URLs)
-#activate :directory_indexes
-#set :trailing_slash, false
+# configure :development do
+#   activate :livereload
+# end
 
 # Methods defined in the helpers block are available in templates
 # helpers do
 #   def some_helper
-#     "Helping"
+#     'Helping'
 #   end
 # end
 
@@ -94,18 +75,9 @@ set :partials_dir, 'partials'
 
 # Build-specific configuration
 configure :build do
-  # For example, change the Compass output style for deployment
+  # Minify CSS on build
   # activate :minify_css
 
   # Minify Javascript on build
   # activate :minify_javascript
-
-  # Enable cache buster
-  # activate :asset_hash
-
-  # Use relative URLs
-  # activate :relative_assets
-
-  # Or use a different image path
-  # set :http_prefix, "/Content/images/"
 end
