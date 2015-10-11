4a0661f7f16237bbee3a3ed0598870c7e8bd9aa4
David Cristofaro <david@dtcristo.com>
2015-10-11 21:24:44 +1100

    Untabify

--- source/javascripts/retina.js
+++ source/javascripts/retina.js
@@ -2,35 +2,34 @@
 // and add a <image-name>@2x.png image. Assumes jquery is loaded.
 
 function isRetina() {
-	var mediaQuery = "(-webkit-min-device-pixel-ratio: 1.5),\
-					  (min--moz-device-pixel-ratio: 1.5),\
-					  (-o-min-device-pixel-ratio: 3/2),\
-					  (min-resolution: 1.5dppx)";
+  var mediaQuery = "(-webkit-min-device-pixel-ratio: 1.5),\
+                    (min--moz-device-pixel-ratio: 1.5),\
+                    (-o-min-device-pixel-ratio: 3/2),\
+                    (min-resolution: 1.5dppx)";
 
-	if (window.devicePixelRatio > 1)
-		return true;
+  if (window.devicePixelRatio > 1)
+    return true;
 
-	if (window.matchMedia && window.matchMedia(mediaQuery).matches)
-		return true;
+  if (window.matchMedia && window.matchMedia(mediaQuery).matches)
+    return true;
 
-	return false;
+  return false;
 };
 
 
 function retina() {
+  if (!isRetina())
+    return;
 
-	if (!isRetina())
-		return;
+  $("img.2x").map(function(i, image) {
 
-	$("img.2x").map(function(i, image) {
+    var path = $(image).attr("src");
 
-		var path = $(image).attr("src");
+    path = path.replace(".png", "@2x.png");
+    path = path.replace(".jpg", "@2x.jpg");
 
-		path = path.replace(".png", "@2x.png");
-		path = path.replace(".jpg", "@2x.jpg");
-
-		$(image).attr("src", path);
-	});
+    $(image).attr("src", path);
+  });
 };
 
 $(document).ready(retina);
