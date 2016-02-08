16cd080b01e7959b46a0a9c5b4121b321c1d05a5
David Cristofaro <git@dtcristo.com>
2016-02-08 21:43:41 +1100

    Update usage of config to support middleman 4

--- helpers/meta_helpers.rb
+++ helpers/meta_helpers.rb
@@ -1,9 +1,9 @@
 module MetaHelpers
   def page_title
-    title = site_title
     if current_article && current_article.title
-      title = current_article.title + ' | ' + site_title
+      current_article.title + ' | ' + config[:site_title]
+    else
+      config[:site_title]
     end
-    title
   end
 end
